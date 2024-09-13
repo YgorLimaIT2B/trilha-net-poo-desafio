2b6e74193254c71f7fd5468f97ebd9cc2685f97e
YgorLimaIT2B <142839028+YgorLimaIT2B@users.noreply.github.com>
2024-09-13 12:21:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Teste no Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia 1", imei: "321", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("/n");
+
+Console.WriteLine("Teste iphone");
+Smartphone iphone = new Iphone(numero: "654321", modelo: "iphone 1", imei: "123", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Zap Zap");
+
